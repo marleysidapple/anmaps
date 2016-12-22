bd168c190fe8b3ceaa2b6c54acbf84c683cdb1d2
= <=>
2016-12-22 20:35:51 +0545

    added navigation

--- app/module/home/home.component.ts
+++ app/module/home/home.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'my-app',
 	template: `this is home`
 })
 
-export class HomeComponent { }
+export class HomeComponent {
+
+
+	constructor(private _router: Router) { }
+
+
+
+
+
+}
