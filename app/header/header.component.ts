6bc739d014580d334bb5e149e35661b4166cbbba
marleysid <bsiddhartha25@gmail.com>
2016-12-23 10:11:03 +0545

    added authentication

--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './../module/auth/auth.service';
 
 @Component({
 	selector: 'header',
-	template: `<h4>This is header</h4><a routerLink='/home' routerLinkActive='active'>Home</a>&nbsp;<a routerLink='/task' routerLinkActive='active'>Task</a>&nbsp;<a routerLink='/todo' routerLinkActive='active'>Todo</a>&nbsp;`
+	template: `{{userDetail | json}}<h4>This is header</h4><a routerLink='/dashboard/home' routerLinkActive='active'>Home</a>&nbsp;<a routerLink='/dashboard/task' routerLinkActive='active'>Task</a>&nbsp;<a routerLink='/dashboard/todo' routerLinkActive='active'>Todo</a>&nbsp;`
 })
 
 
-export class HeaderComponent { }
+export class HeaderComponent {
+
+	userDetail: string;
+
+	ngOnInit() {
+		this.getCurrentUser();
+	}
+
+
+	constructor(private authService: AuthService) { }
+
+
+	getCurrentUser() {
+		this.authService.getCurrentUser(localStorage.getItem('currentUser')).subscribe(
+			(result) => {
+					if (result){
+						this.userDetail = result;
+						//console.log(JSON.stringify(result.token));
+						//console.log(result);
+					}
+				},
+
+			err => {
+				this.errMessage = 'this is an err';
+			},
+
+			() => {}
+
+		);
+	}
+
+
+}
--- app/module/auth/auth.service.ts
+++ app/module/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 
@@ -19,8 +19,20 @@ export class AuthService {
 
 	postLogin(body: Object): Observable<any> {
 		return this._http.post(this.url + '/auth/login', body)
-			.map((res:Response) => res.json()) // ...and calling .json() on the response to return data
-            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+			.map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+	}
+
+
+	getCurrentUser(token: string): Observable<any> {
+		let options = new RequestOptions({
+			search: new URLSearchParams('token='+token)
+		});
+
+
+		return this._http.get(this.url + '/getcurrentuser', options)
+			.map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
 	}
 
 
